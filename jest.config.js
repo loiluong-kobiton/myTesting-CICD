d697bce40edfb747cff9c68c35222d9de4c30c2a
loiluong <138481467+loiluong-kobiton@users.noreply.github.com>
2023-07-12 15:43:18 +0700

    test new

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    coverageThreshold: {
+        global: {
+            lines: 80,
+            branches: 80,
+            function: 80,
+            statements: 80
+        },
+    },
+};
