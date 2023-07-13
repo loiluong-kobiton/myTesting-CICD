9393770ee8e6a0e4aafc5d8611dd7ee24256ea76
loiluong <138481467+loiluong-kobiton@users.noreply.github.com>
2023-07-13 17:28:21 +0700

    fix running npx not yarn

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,24 @@
+// const gulp = require('gulp');
+// const eslint = require('gulp-eslint');
+// const sourcemaps = require('gulp-sourcemaps');
+
+
+// gulp.task('lint', () => {
+//   return gulp
+//     .src(['src/**/*.js']) 
+//     .pipe(eslint())
+//     .pipe(eslint.format());
+// });
+
+
 const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const sourcemaps = require('gulp-sourcemaps');
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
 
-
-gulp.task('lint', () => {
-  return gulp
-    .src(['src/**/*.js']) 
-    .pipe(eslint())
-    .pipe(eslint.format());
+gulp.task('lint', function() {
+  return gulp.src('src/index.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
 });
+
+gulp.task('default', gulp.series('lint'));
