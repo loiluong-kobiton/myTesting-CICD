4767cb862d2f74a06631a0ea59a55ffbba832930
loiluong <138481467+loiluong-kobiton@users.noreply.github.com>
2023-07-14 16:33:37 +0700

    fix jest config file and set coverage to 50

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,24 @@
+// module.exports = {
+//     coverageThreshold: {
+//       global: {
+//         lines: 80,
+//         branches: 80,
+//         function: 80,
+//         statements: 80,
+//       },
+//     },
+//   };
+
 module.exports = {
-    coverageThreshold: {
-      global: {
-        lines: 80,
-        branches: 80,
-        function: 80,
-        statements: 80,
-      },
+coverageThreshold: {
+    global: {
+    lines: 50,
+    branches: 50,
+    function: 50,
+    statements: 50,
     },
-  };
+},
+};
 
 
 
