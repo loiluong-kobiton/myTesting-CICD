d6e2a8d3e5a108a9b341bd395d48f58647f58124
loiluong <138481467+loiluong-kobiton@users.noreply.github.com>
2023-07-14 11:41:36 +0700

    test get status when coverage is 80

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 const config = {
     "coverageThreshold": {
         "global": {
-            // "lines": 80,
-            // "branches": 80,
-            // "function": 80,
-            // "statements": 80
+            "lines": 80,
+            "branches": 80,
+            "function": 80,
+            "statements": 80
 
-            lines: 50,
-            branches: 50,
-            function: 50,
-            statements: 50
+            // lines: 50,
+            // branches: 50,
+            // function: 50,
+            // statements: 50
         },
     }
     // verbose: true,
