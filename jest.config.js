50865ddf71481f8fa5a58f1a559d530cadb4f116
loiluong <138481467+loiluong-kobiton@users.noreply.github.com>
2023-07-14 10:33:48 +0700

    fix jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,44 @@
-module.exports = {
-    coverageThreshold: {
-        global: {
-            // lines: 80,
-            // branches: 80,
-            // function: 80,
-            // statements: 80
+// const config = {
+//     "coverageThreshold": {
+//         "global": {
+//             "lines": 80,
+//             "branches": 80,
+//             "function": 80,
+//             "statements": 80
 
-            lines: 50,
-            branches: 50,
-            function: 50,
-            statements: 50
-        },
-    },
-};
+//             // lines: 50,
+//             // branches: 50,
+//             // function: 50,
+//             // statements: 50
+//         },
+//     },
+//     // verbose: true,
+// };
+
+// module.exports = config;
+
+
+// "jest": {
+//     "coverageThreshold": {
+//       "global": {
+//         "branches": 75,
+//         "functions": 75,
+//         "lines": 75,
+//         "statements": 75
+//       }
+//     }
+//   },
+
+// module.exports = {
+//     testEnvironment: 'node',
+    
+//     testMatch: [
+//       '<rootDir>/__tests__/**/*.js'
+//     ],
+    
+//     collectCoverageFrom: [
+//       '<rootDir>/src/**/*.js'
+//     ],
+//     coverageReporters: ['text-summary', 'json-summary', 'lcov']
+//   }
+  
